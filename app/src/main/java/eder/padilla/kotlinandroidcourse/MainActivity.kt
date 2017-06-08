001c5585272c634b3192573cd7e055fa76caff59
edersinsinsin <eder.padilla97@gmail.com>
2017-06-08 17:40:42 -0500

    ejemplo de como hacer un on click

--- app/src/main/java/eder/padilla/kotlinandroidcourse/MainActivity.kt
+++ app/src/main/java/eder/padilla/kotlinandroidcourse/MainActivity.kt
@@ -2,13 +2,23 @@ package eder.padilla.kotlinandroidcourse
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.IntegerRes
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mTextView.setText("Hellopin my friends")
+    }
+    protected fun getAgeEvent(view: View){
+        val age : String= mEtGetAge.text.toString()
+        val year : Int = Calendar.getInstance().get(Calendar.YEAR)
+        val myAge = year-(Integer.valueOf(age))
+        mTvAge.text = "Tu edad es $myAge"
     }
 }
