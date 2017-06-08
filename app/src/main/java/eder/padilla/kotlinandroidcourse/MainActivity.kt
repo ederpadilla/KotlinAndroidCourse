7a3f4ea0fa5867872c0fe979dde0fe6fc81df440
edersinsinsin <eder.padilla97@gmail.com>
2017-06-07 23:55:27 -0500

    Initial commit

---
+++ app/src/main/java/eder/padilla/kotlinandroidcourse/MainActivity.kt
@@ -0,0 +1,14 @@
+package eder.padilla.kotlinandroidcourse
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mTextView.setText("Hellopin my friends")
+    }
+}
